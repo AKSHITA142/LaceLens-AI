cdd66d657dd0bc396acff0ddfdf9f0a5e3530960
AKSHITA142 <akshitajariwala07@gmail.com>
2026-01-01 18:26:46 +0530

    Restructure project into monorepo (frontend + backend)

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("SAGARAM LACE BACKEND RUNNING 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
