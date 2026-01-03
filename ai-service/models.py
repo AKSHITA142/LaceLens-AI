c193a7f8b0e2ef001696e5200bc89394f8c381e3
AKSHITA142 <akshitajariwala07@gmail.com>
2026-01-03 17:04:08 +0530

    Succesfully Write ai-service (upload and search runs sucessfully)

--- ai-service/models.py
+++ ai-service/models.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, Integer, Text, TIMESTAMP
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from database import Base
 
 class Lace(Base):
     __tablename__ = "laces"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(Text, nullable=False)
-    image_path = Column(Text, nullable=False)
-    created_at = Column(TIMESTAMP)
+    name = Column(String, nullable=False)
+    image_path = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
 
